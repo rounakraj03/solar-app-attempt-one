59a1354a9119c8a8e4b2ee7eee65bf60edd7b4ab
rounak-stellar <rounakraj@infostellar.com>
2024-03-16 10:43:16 +0530

    customer review slider

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ananta Solar",
+  title: "Ananta Power Tech",
   description: "Best Solar in India | Best Solar Panel in India | India best solar panel | Solar Panel | Cheapest Solar Panel in India | Best Solar Panel | Solar Panel",
 };
 
